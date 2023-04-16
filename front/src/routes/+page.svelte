6623d55d48df76e35c9aefaf6aad923a991076ff
Daniel Cui <solunian@gmail.com>
2023-04-16 00:10:02 -0700

    bind values for form

--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
   import { onMount } from "svelte";
+  import crimes from "$lib/crime.json";
 
   let latitude;
   let longitude;
@@ -15,34 +16,40 @@
     }
   });
 
-  const reportHandler = (e) => {};
+  let title, desc;
+  let categories = crimes.crimes;
+
+
+  const reportHandler = (e) => {
+    
+  };
 </script>
 
 <div class="flex flex-row justify-center my-10">
-  <form
-    on:submit|preventDefault={reportHandler}
-    class="flex flex-col w-96 border rounded p-5 gap-4"
-  >
-    <label for="title"
-      >Title:
-      <input name="title" type="text" value="" class="border rounded-sm" />
+  <form on:submit|preventDefault={reportHandler} class="flex flex-col w-96 border rounded p-5 gap-4">
+    <label for="title">
+      Title:
+      <input name="title" type="text" bind:value={title} class="border rounded-sm" />
     </label>
 
     <div class="row row-col">
       <label for="desc" class="block">Description:</label>
       <textarea id="desc" name="text" oninput='this.style.height = "";this.style.height = this.scrollHeight + "px"'
-      class="border rounded-sm w-full resize-none"></textarea>
+      class="border rounded-sm w-full resize-none" bind:value={desc}></textarea>
     </div>
 
     <div>
       <label for="categories">Categories:</label>
       <select name="categories" class="border rounded-sm">
-        <option value="">Assault and Pepper</option>
-        <option value="">Sexual Assault</option>
-        <option value="">Armed Assault</option>
-        <option value="">Amber Alert Sighting</option>
-        <option value="">Burglary</option>
+        {#each categories as category}
+          <option value="">{category}</option>
+        {/each}
       </select>
+    </div>
+
+    <div>
+      <label for="image">Images</label>
+      <input type="file" accept="image/png, image/jpeg"/>
     </div>
 
     <button type="submit" class="border rounded-sm">Report</button>
