9e3c62b5d5b19a079bb37b670281b3cb965fd23a
jetstream0 <113315327+downloadablecontent@users.noreply.github.com>
2023-04-15 21:25:36 -0700

    fix

--- back/index.js
+++ back/index.js
@@ -6,13 +6,13 @@ app.get("/test", (req, res) => {
   return res.json({"hey": "i just met you"});
 });
 
-app.post("/upload", (req, res) => {
+app.post("/upload", async (req, res) => {
   console.log("upload request received, extracted body: ", req.body);
   let _success = await controller.upload(req.body);
   return res.status(201); 
 });
 
-app.get("/by-location", (req, res) => {
+app.get("/by-location", async (req, res) => {
   console.log("get by location requested, still dummy tho.");
   const location = { lat: 32, lon: -121};
   let _results = await controller.getByLocation(location);
