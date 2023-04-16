48a97fda4d485c324c97f0af245a23aaf1bb08a2
Daniel Cui <solunian@gmail.com>
2023-04-16 01:20:29 -0700

    about page created

--- front/src/lib/components/Navbar.svelte
+++ front/src/lib/components/Navbar.svelte
@@ -1,6 +1,5 @@
-<nav class="flex flex-row z-50 justify-center gap-14 bg-slate-600 text-gray-50 p-4">
+<nav class="flex flex-row z-50 justify-center gap-14 bg-slate-600 text-gray-50 p-4 shadow-slate-400 shadow">
     <a href="/">REPORT</a>
     <a href="/map">MAP</a>
     <a href="/about">ABOUT</a>
-
 </nav>
--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -21,7 +21,7 @@
 
 
   const reportHandler = (e) => {
-    
+
   };
 </script>
 
