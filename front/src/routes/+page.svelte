2692ac6edfdc5e105b94090528d5efd2ce10c7e2
Daniel Cui <solunian@gmail.com>
2023-04-15 23:46:34 -0700

    some ui stuff nicer?

--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -1,41 +1,42 @@
 <script>
-  import { onMount } from 'svelte';
+  import { onMount } from "svelte";
 
   let latitude;
   let longitude;
 
   onMount(() => {
-    console.log(navigator)
+    console.log(navigator);
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition((pos) => {
-        console.log(pos.coords)
+        console.log(pos.coords);
         latitude = pos.latitude;
         longitude = pos.longitude;
       });
     }
-  })
+  });
 
-  const reportHandler = (e) => {
-
-  };
+  const reportHandler = (e) => {};
 </script>
 
-
 <div class="flex flex-row justify-center my-10">
-  <form on:submit|preventDefault={reportHandler} class="flex flex-col w-96 border rounded p-5 gap-4">
-
-    <label for="title">Title:
-      <input name="title" type="text" value="" class="border rounded-sm"/>
+  <form
+    on:submit|preventDefault={reportHandler}
+    class="flex flex-col w-96 border rounded p-5 gap-4"
+  >
+    <label for="title"
+      >Title:
+      <input name="title" type="text" value="" class="border rounded-sm" />
     </label>
 
     <div class="row row-col">
       <label for="desc" class="block">Description:</label>
-      <textarea name="desc" type="text" value="" class="border rounded-sm w-full"/>
+      <textarea id="desc" name="text" oninput='this.style.height = "";this.style.height = this.scrollHeight + "px"'
+      class="border rounded-sm w-full resize-none"></textarea>
     </div>
 
     <div>
       <label for="categories">Categories:</label>
-      <select name="categories">
+      <select name="categories" class="border rounded-sm">
         <option value="">Assault and Pepper</option>
         <option value="">Sexual Assault</option>
         <option value="">Armed Assault</option>
@@ -44,8 +45,12 @@
       </select>
     </div>
 
-    <button type="submit">Report</button>
+    <button type="submit" class="border rounded-sm">Report</button>
   </form>
 </div>
 
-  
+<style>
+  #desc {
+    resize: none;
+  }
+</style>
